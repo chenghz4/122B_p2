b7a99211d24e4e9ce29755e726d3a58b9946ea8a
Yicong-Huang <hyc541978023@gmail.com>
2018-04-16 08:06:17 -0700

    added comments

--- WebContent/index.js
+++ WebContent/index.js
@@ -1,22 +1,56 @@
+/**
+ * This example is following frontend and backend separation.
+ *
+ * Before this .js is loaded, the html skeleton is created.
+ *
+ * This .js performs two steps:
+ *      1. Use jQuery to talk to backend API to get the json data.
+ *      2. Populate the data to correct html elements.
+ */
+
+
+/**
+ * Handles the data returned by the API, read the jsonObject and populate data into html elements
+ * @param resultData jsonObject
+ */
 function handleStarResult(resultData) {
     console.log("handleStarResult: populating star table from resultData");
 
     // populate the star table
+
+    // find the empty table body by id "star_table_body"
     let starTableBodyElement = jQuery("#star_table_body");
+
+    // iterate through resultData, no more than 10 entries
     for (let i = 0; i < Math.min(10, resultData.length); i++) {
+
+        // concatenate the html tags with resultData jsonObject
         let rowHTML = "";
         rowHTML += "<tr>";
-        rowHTML += "<th>" + '<a href="/project2-api-example/single-star.html?id=' + resultData[i]['star_id'] + '">' + resultData[i]["star_name"] + '</a>' + "</th>";
+        rowHTML +=
+            "<th>" +
+            // add a link to single-star.html with id passed with GET url parameter
+            '<a href="/project2-api-example/single-star.html?id=' + resultData[i]['star_id'] + '">'
+            + resultData[i]["star_name"] +     // display star_name for the link text
+            '</a>' +
+            "</th>";
         rowHTML += "<th>" + resultData[i]["star_dob"] + "</th>";
         rowHTML += "</tr>";
+
+        // append the row created to the table body, which will refresh the page
         starTableBodyElement.append(rowHTML);
     }
 }
 
+
+/**
+ * Once this .js is loaded, following scripts will be executed by the browser
+ */
+
 // makes the HTTP GET request and registers on success callback function handleStarResult
 jQuery.ajax({
-    dataType: "json",
-    method: "GET",
-    url: "/project2-api-example/stars",
-    success: (resultData) => handleStarResult(resultData)
+    dataType: "json", // setting return data type
+    method: "GET", // setting request method
+    url: "/project2-api-example/stars", // setting request url, which is mapped by StarsServlet in Stars.java
+    success: (resultData) => handleStarResult(resultData) // setting callback function to handle data returned successfully by the StarsServlet
 });
--- WebContent/single-star.js
+++ WebContent/single-star.js
@@ -1,31 +1,61 @@
-// Retrieve parameter from request URL, matching by parameter name
-function getParameterByName(name, url) {
-    // Get request URL
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
+/**
+ * This example is following frontend and backend separation.
+ *
+ * Before this .js is loaded, the html skeleton is created.
+ *
+ * This .js performs three steps:
+ *      1. Get parameter from request URL so it know which id to look for
+ *      2. Use jQuery to talk to backend API to get the json data.
+ *      3. Populate the data to correct html elements.
+ */
 
-    // Ues regular expression to find matched parameter
-    let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+
+/**
+ * Retrieve parameter from request URL, matching by parameter name
+ * @param target String
+ * @returns {*}
+ */
+function getParameterByName(target) {
+    // Get request URL
+    let url = window.location.href;
+    // Encode target parameter name to url encoding
+    target = target.replace(/[\[\]]/g, "\\$&");
+
+    // Ues regular expression to find matched parameter value
+    let regex = new RegExp("[?&]" + target + "(=([^&#]*)|&|#|$)"),
         results = regex.exec(url);
     if (!results) return null;
     if (!results[2]) return '';
 
-    // Return the decoded parameter
+    // Return the decoded parameter value
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
+/**
+ * Handles the data returned by the API, read the jsonObject and populate data into html elements
+ * @param resultData jsonObject
+ */
+
+//
 function handleResult(resultData) {
 
     console.log("handleResult: populating star info from resultData");
+
     // populate the star info h3
+    // find the empty h3 body by id "star_info"
     let starInfoElement = jQuery("#star_info");
+
+    // append two html <p> created to the h3 body, which will refresh the page
     starInfoElement.append("<p>Star Name: " + resultData[0]["star_name"] + "</p>" +
         "<p>Date Of Birth: " + resultData[0]["star_dob"] + "</p>");
 
     console.log("handleResult: populating movie table from resultData");
 
     // populate the star table
+    // find the empty table body by id "movie_table_body"
     let movieTableBodyElement = jQuery("#movie_table_body");
+
+    // concatenate the html tags with resultData jsonObject to create table rows
     for (let i = 0; i < Math.min(10, resultData.length); i++) {
         let rowHTML = "";
         rowHTML += "<tr>";
@@ -33,16 +63,22 @@ function handleResult(resultData) {
         rowHTML += "<th>" + resultData[i]["movie_year"] + "</th>";
         rowHTML += "<th>" + resultData[i]["movie_director"] + "</th>";
         rowHTML += "</tr>";
+
+        // append the row created to the table body, which will refresh the page
         movieTableBodyElement.append(rowHTML);
     }
 }
 
+
+// Once this .js is loaded, following scripts will be executed by the browser
+
 // Get id from URL
 let starId = getParameterByName('id');
 
-// makes the HTTP GET request and registers on success callback function handleStarResult
+// makes the HTTP GET request and registers on success callback function handleResult
 jQuery.ajax({
-    dataType: "json",
-    method: "GET",
-    url: "/project2-api-example/SingleStar?id=" + starId, success: (resultData) => handleResult(resultData)
+    dataType: "json",  // setting return data type
+    method: "GET",// setting request method
+    url: "/project2-api-example/SingleStar?id=" + starId, // setting request url, which is mapped by StarsServlet in Stars.java
+    success: (resultData) => handleResult(resultData) // setting callback function to handle data returned successfully by the SingleStarServlet
 });
